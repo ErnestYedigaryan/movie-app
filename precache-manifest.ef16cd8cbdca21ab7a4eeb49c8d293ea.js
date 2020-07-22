0dd94f463944af063346a983879c47f027a67d7f
Ernest Yedigaryan <ernestyedigaryan@gmail.com>
2020-07-22 17:27:21 +0400

    Updates

---
+++ precache-manifest.ef16cd8cbdca21ab7a4eeb49c8d293ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0cad824cfd2974e8ef9ad7b996f5a82",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "f588bc0fe0083fe8d98e",
+    "url": "/movie-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d7a91c6ea392860a7bb9",
+    "url": "/movie-app/static/js/2.51e385c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.51e385c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f588bc0fe0083fe8d98e",
+    "url": "/movie-app/static/js/main.05ccdbc0.chunk.js"
+  },
+  {
+    "revision": "8c0fe60c15a349e095c4",
+    "url": "/movie-app/static/js/runtime-main.1fbae34b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movie-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
